c68443d794f727b0ab6a16404780be58b0672088
pin1114 <103117436+pin1114@users.noreply.github.com>
2025-02-03 21:28:10 +0800

    update

---
+++ js/index.js
@@ -0,0 +1,28 @@
+//init
+$('#Aside').hide();
+
+$(window).on('scroll', function(){
+    //  console.log($(window).scrollTop())
+
+    if($(window).scrollTop() > 300){
+        $('#Aside').fadeIn();
+    }
+    else{
+        $('#Aside').fadeOut();
+    }
+
+    if($(window).scrollTop() != 0){
+        $('#Header').addClass('header-active');
+    }
+    else{
+        $('#Header').removeClass('header-active');
+    }
+});
+
+$('#BtnBars').on('click', function(){
+    $('#HeaderBody').fadeIn();
+})
+
+$('#BtnXmark').on('click', function(){
+    $('#HeaderBody').fadeOut();
+})
